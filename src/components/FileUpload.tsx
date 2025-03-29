518a49ab449b3e75a786c98abd14e9ccf03d7a6e
zb0505 <bazska23@gmail.com>
2025-03-29 21:38:57 +0100

    Fixed a bug regarding view for MeTRAbs/CapeX

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,5 +1,5 @@
 import { Component, ReactNode } from "react"
-import { Utils } from "../api_tools"
+import API, { Utils } from "../api_tools"
 import KeypointMarker from "./KeypointMarker"
 import { AppState } from "../App"
 import Modal from "./Modal"
@@ -141,7 +141,7 @@ export default class FileUpload extends Component<FileUploadProps, FileUploadSta
 						</KeypointMarker>
 					</div>
 				</div>
-				<div className="mw-45 ms-0 ms-lg-5 mt-3 mt-lg-0">
+				<div className="mw-45 ms-0 ms-lg-5 mt-3 mt-lg-0" hidden={API.skeletonModel !== "capex"}>
 					<h5 className="form-label m-0 my-2">Keypoints</h5>
 					<p className="mb-2 text-start">
 						List all keypoints in order and adjust connections with the number of dashes.
